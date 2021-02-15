84698b89f4beb49f3ccc37c12aeabec25e180c25
Jean Rodrigues <jeanrodrigues002@gmail.com>
2021-02-14 22:22:44 -0300

    Atualizando

---
+++ links/jslink/curriculo/informacao.js
@@ -0,0 +1,20 @@
+function toggle(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    const popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+function toggle2(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    const social = document.getElementById('social');
+    social.classList.toggle('active');
+}
+ 
+function toggle3(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    const habi = document.getElementById('habi');
+    habi.classList.toggle('active');
+}
