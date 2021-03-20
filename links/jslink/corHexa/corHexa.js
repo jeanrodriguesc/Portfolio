b98c6c2e6934e393ba3a71d41afa4edd519b16e6
Jean Rodrigues <jeanrodrigues002@gmail.com>
2021-03-20 16:48:18 -0300

    Colocando Cor em Hexa

---
+++ links/jslink/corHexa/corHexa.js
@@ -0,0 +1,11 @@
+function myColor(){
+    var color = document.getElementById('colorPanel').value;
+    document.body.style.backgroundColor = color;
+    document.getElementById('box').value = color;    
+}
+
+function myColor2(){
+    var color = document.getElementById('colorPanel').value;
+    document.body.style.backgroundColor = color;
+    document.getElementById('box').value = color;
+}
