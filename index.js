c9bdb5cfdcbac1ec4b90217e6f6b3cfc2d6a78e2
Jean Rodrigues <jeanrodrigues002@gmail.com>
2021-03-31 15:51:13 -0300

    teste

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
 
-app.get("/", (request, response) => {
-    response.send("Oi heroku")
-});
+// app.get("/", (request, response) => {
+//     response.send("Oi heroku")
+// });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log("rodando na porta" + PORT);
-})
+// app.listen(PORT, () => {
+//     console.log("rodando na porta" + PORT);
+// })
+
+module.exports = {
+    port: process.env.PORT,
+    files: ["./**/*.{html.htm,css,js}"],
+    server:{
+        baseDir: ["./src", "index.html"]
+    }
+}
