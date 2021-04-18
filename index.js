74f3ff772c25093322526c882251450361f9679f
Jean Rodrigues <jeanrodrigues002@gmail.com>
2021-04-18 12:37:32 -0300

    teste heroku

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// const express = require("express")
-// const app = express()
+const express = require("express")
+const app = express()
 
-// app.get("/", (request, response) => {
-//     response.send("Oi heroku")
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//     console.log("rodando na porta" + PORT);
-// })
-
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static('final'));
-
-
-app.listen(port, function () {
-  console.log('Started on port ' + port)
+app.get("/", (request, response) => {
+    response.send("Oi heroku")
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("rodando na porta" + PORT);
+})
+
+
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static('final'));
+
+
+// app.listen(port, function () {
+//   console.log('Started on port ' + port)
+// });
