e10ee054099d8e3e802dd417d94cbc2c00257880
Jean Rodrigues <jeanrodrigues002@gmail.com>
2021-05-04 11:24:13 -0300

    teste heroku 6

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+//Importando
 const express = require("express")
 const app = express()
+const PORT = (process.env.PORT || 8080);
 
-app.get("/", (request, response) => {
-    response.send("Oi heroku")
+//arquivos
+
+app.use(express.static()) 
+app.use('/css', express.static(__dirname + 'css'))
+app.use('/js', express.static(__dirname + 'js'))
+app.use('/html', express.static(__dirname + 'html'))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
 });
 
-const PORT = (process.env.PORT || 5000);
-
 app.listen(PORT, () => {
-    console.log("rodando na porta" + PORT);
+    console.log("rodando na porta " + PORT);
 })
 
+
+
+
+//----------------------------------------
+
+// const express = require("express")
+// const app = express()
+
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/index.html")
+// });
+
+// const PORT = (process.env.PORT || 8080);
+
+// app.listen(PORT, () => {
+//     console.log("rodando na porta" + PORT);
+// })
+
+
+//-----------------------------
 
 // const express = require('express');
 // const app = express();
